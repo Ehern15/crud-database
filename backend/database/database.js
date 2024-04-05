41660707e04dd80e37736af013a52ce535f26f1a
Ehern15 <erikhern15@gmail.com>
2024-04-05 17:51:37 -0400

    got the register page to register a user to the database

---
+++ backend/database/database.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '!9Dayo2390',
+  database: 'crud_project',
+})
+
+
--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,40 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const mysql = require('mysql')
+const cors = require('cors');
+const db = require('./database/database');
 
+const port = process.env.PORT || 5000;
 
-const loginRoute = require('./routes/Login');
-const registerRoute = require('./routes/Register');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
 
-app.use('/login', loginRoute);
-app.use('/register', registerRoute);
-
-app.post('/register', (req, res) => {
-  const {firstName,lastName,email,userName,password} = req.body;
-  
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '!9Dayo2390',
+  database: 'crud_project',
 })
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello, Express.js Server!</h1>');
+connection.connect();
+//register a user
+app.post('/register', (req, res) => {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+  const username = req.body.username;
+  const password = req.body.password;
+  
+  connection.query('INSERT INTO users (username,first_name,last_name,password,email) VALUES (?, ?, ?, ?, ?)', [username,firstName,lastName,password,email], (err, rows, fields) => {
+    if (err) throw err
+
+  });
 });
-const port = process.env.PORT || 3000;
+
+//TODO: login
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
